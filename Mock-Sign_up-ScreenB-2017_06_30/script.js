4520a92ba92a2634f1e8c6e8bfc9db7f7f7d079b
Vladimir Grigore <vladimirgrigore@Vladimirs-MacBook-Pro.local>
2017-07-03 13:10:21 -0700

    Add animation to drop-down menu

--- Mock-Sign_up-ScreenB-2017_06_30/script.js
+++ Mock-Sign_up-ScreenB-2017_06_30/script.js
@@ -1,5 +1,35 @@
 $(document).ready(function(e) {
 
   // Make the image map responsive
-  $('img[usemap]').rwdImageMaps();
+  $("img[usemap]").rwdImageMaps();
+
+  $("#menu-button").on("click", function(){
+    // Animation end triggers
+    var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+
+    if($("#menu").hasClass("hidden")){
+
+      // Display the menu and background blur, enable animation
+      $("#menu").removeClass("hidden").addClass("animated slideInDown").one(animationEnd, function(){
+        // Disable animation on completion
+        $(this).removeClass("animated slideInDown");
+      });
+      $(".image-blur").removeClass("hidden").addClass("animated fadeIn").one(animationEnd, function(){
+        $(this).removeClass("animated fadeIn");
+      });
+
+    } else {
+
+      // Remove the menu and background blur, enable animation
+      $("#menu").addClass("animated slideOutUp").one(animationEnd, function(){
+        // Disable animation on completion and hide the element
+        $(this).removeClass("animated slideOutUp").addClass("hidden");
+      });
+      $(".image-blur").addClass("animated fadeOut").one(animationEnd, function(){
+        $(this).removeClass("animated fadeOut").addClass("hidden");
+      });
+
+    }
+  });
+
 });
