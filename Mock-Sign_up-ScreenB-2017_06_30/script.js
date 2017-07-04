c880831a93ae1fe6d94d7b2cb9b370e7f40dc497
Vladimir Grigore <vladimirgrigore@Vladimirs-MacBook-Pro.local>
2017-07-04 12:42:50 -0700

    Add comments

--- Mock-Sign_up-ScreenB-2017_06_30/script.js
+++ Mock-Sign_up-ScreenB-2017_06_30/script.js
@@ -3,12 +3,13 @@ $(document).ready(function(e) {
   // Make the image map responsive
   $("img[usemap]").rwdImageMaps();
 
+  // Open/close drop-down menu
   $("#menu-button").on("click", function(){
     // Animation end triggers
     var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
 
+    // Check if the menu is already displayed
     if($("#menu").hasClass("hidden")){
-
       // Display the menu and background blur, enable animation
       $("#menu").removeClass("hidden").addClass("animated slideInDown").one(animationEnd, function(){
         // Disable animation on completion
@@ -17,9 +18,7 @@ $(document).ready(function(e) {
       $(".image-blur").removeClass("hidden").addClass("animated fadeIn").one(animationEnd, function(){
         $(this).removeClass("animated fadeIn");
       });
-
     } else {
-
       // Remove the menu and background blur, enable animation
       $("#menu").addClass("animated slideOutUp").one(animationEnd, function(){
         // Disable animation on completion and hide the element
@@ -28,8 +27,8 @@ $(document).ready(function(e) {
       $(".image-blur").addClass("animated fadeOut").one(animationEnd, function(){
         $(this).removeClass("animated fadeOut").addClass("hidden");
       });
-
     }
+
   });
 
 });
